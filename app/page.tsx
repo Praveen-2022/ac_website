5fe404c13d8e16b1510c7b7850c8bcb3ba2fccde
Praveen Kumar Chauhan <praveenkumarchauhan2022@gmail.com>
2024-01-03 10:57:14 +0530

    adding components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import Image from "next/image";
+import Banner from "@/components/Banner";
+import GasfillingCharges from "@/components/GasfillingCharges";
+import Footer from "@/components/shared/Footer";
+import Navbar from "@/components/shared/Navbar";
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
+      {/* hero Section
+      Safety Section
+      Ous Services
+      Our Products
+      Services We Offer
+      About Us
+      Banner
+      Footer */}
       <h1>Namrata Enterprises</h1>
+      <Banner />
+      <GasfillingCharges/>
+      <Footer />
     </main>
   );
 }
